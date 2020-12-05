db9ef48a809d5fcd2f1634a49afdcae4a1b3f9d4
Bartosz Nowicki <b_nowicki@op.pl>
2020-12-06 00:01:16 +0100

    pieces loaded

--- src/Loader.js
+++ src/Loader.js
@@ -1,8 +1,5 @@
 import { TheDOM, DATA } from './TheDOM.js';
-import loaderPicHTML from './assets/king.svg';
-import startPicHTML from './assets/start.svg';
-import piecesPicHTML from './assets/pieces.svg';
-import kingPicHTML from './assets/king.svg';
+import allPiecesPicHTML from './assets/pieces.svg';
 import img1 from './assets/1.jpg';
 import img2 from './assets/2.jpg';
 import img3 from './assets/3.jpg';
@@ -14,71 +11,91 @@ class Loader extends TheDOM {
         super(DATA.LOADER_SELECTOR);
         this.areAllItemsLoaded = false;
 		this.loadedItemsCounter = 0;
-		this.importedSvgHTML = [loaderPicHTML, startPicHTML, piecesPicHTML, kingPicHTML];
+		this.importedSvgHTML = [allPiecesPicHTML];
 		this.importedPicReferences = [img1, img2, img3, img4, img5];
-		this.importedPics = [];
+		this.allImportedItems = [...this.importedSvgHTML, ...this.importedPicReferences];
 		this.width = 0;
 		this.height = 0;
         this.initializeLoader();
-	}
+	}	
 
 	resizeGameWindow() {
 		const { innerWidth: width, innerHeight: height } = window;
 		const scale = Math.min(width / DATA.CANVAS_BASE_WIDTH, height / DATA.CANVAS_BASE_HEIGHT);
 		console.log(width, height, DATA.CANVAS_BASE_WIDTH, DATA.CANVAS_BASE_HEIGHT);		
 		document.documentElement.style.setProperty("--scaleValue", scale);
-		console.log('GAME WINDOW RESIZED AGAINST BASE 360 X 640 BY SCALE: ', scale);
-		
+		console.log('GAME WINDOW RESIZED AGAINST BASE 360 X 640 BY SCALE: ', scale);		
 	}
 
-	loadImage(imageUrl) {
-		// console.log('trying to load: ', imageUrl);
+	loadImage(imageReference) {		
 		const image = new Image();
-		image.src = imageUrl;
-		// console.log('image source: : ', image.src);
-		image.addEventListener('load', event => this.itemLoaded(event), false);        
-		return image;
+		image.src = imageReference;
+		image.setAttribute('class', 'loadingTest');		
+		this.element.appendChild(image);
+		image.addEventListener('load', event => this.imageLoaded(event), false); 
+	}
+
+	loadSvg(svgHTML) {		
+		const newDiv = document.createElement('div');
+		newDiv.setAttribute('class', 'loadingTest');
+		newDiv.innerHTML = svgHTML;
+		newDiv.style.left = `${this.loadedItemsCounter * 0}px`;
+		this.element.appendChild(newDiv);
+		this.loadedItemsCounter++;
+		// console.log('LOADED HTML: ', newDiv.innerHTML);
+		console.log(newDiv);
+		// debugger
+		
 	}
 
     finalItemLoaded() {
         console.log('ALL ITEMS LOADED');
 		this.areAllItemsLoaded = true;
-		this.importedPics.forEach(item => {
-			this.toggleVisibility(item, 'invisible');
-			});
-		this.toggleVisibility(this.element, 'invisible');
-		//START TEST IF IT BECOMES INVISIBLE
-		// const displayProp = window.getComputedStyle(this.element).getPropertyValue('display');
-		// console.log(displayProp);
-		//END TEST IF IT BECOMES INVISIBLE
-		window.dispatchEvent(new CustomEvent(ITEMSLOADED_EVENT_NAME));
+		// this.toggleVisibility(this.element, 'invisible');
+		// window.dispatchEvent(new CustomEvent(ITEMSLOADED_EVENT_NAME));
 	}	
 
-	itemLoaded(event) {
-		// console.log('loaded item: ', event.target);
-		event.target.removeEventListener(event.type, this.itemLoaded, false);
+	imageLoaded(event) {
+		event.target.removeEventListener(event.type, this.imageLoaded, false);
 		this.loadedItemsCounter++;
-		const loadedFraction = this.loadedItemsCounter / this.importedPicReferences.length;
-		this.element.style.backgroundPosition = `${this.width * (1 - loadedFraction)}px center`;
+		event.target.style.left = `${this.loadedItemsCounter * 50}px`;
+		const loadedFraction = Math.floor(this.loadedItemsCounter / this.allImportedItems.length * 100);
 		console.log('FRACTION OF LOADED ITEMS: ', loadedFraction);
-		// this.element.innerHTML += this.loadedItemsCounter;
-		// event.target.classList.add('loadingTest');
-		event.target.setAttribute('class', 'loadingTest');
-		event.target.style.left = `${this.loadedItemsCounter * 60}px`;
-		this.element.appendChild(event.target);
-		this.importedPics.push(event.target);
-
-		if (this.loadedItemsCounter === this.importedSvgHTML.length) {
+		let loaderInfoMessage = "'loading pieces...'" + `"  ${loadedFraction} %"`;
+		document.documentElement.style.setProperty("--loaderInfo", `${loaderInfoMessage}`);
+		
+		if (this.loadedItemsCounter === this.allImportedItems.length) {
 			this.finalItemLoaded();
 		}
-
 	}	
-
+	
     consoleInitialInfo() {
         console.log('loader constructor here');
 		console.log('loader element: ', this.element);
         console.log('loader this: ', this);
-    }
+	}	
+
+	loadAllItems() {
+		for (let i = 0; i <= this.importedPicReferences.length - 1; i++) {
+			this.loadImage(this.importedPicReferences[i]);			
+		}
+		for (let j = 0; j <= this.importedSvgHTML.length - 1; j++) {
+			this.loadSvg(this.importedSvgHTML[j]);			
+		}
+	}
+
+	insertPiece(piece, parent) {
+		const chessPiece = document.getElementById(piece);
+		const chessPieceDiv = document.createElement('div');
+		parent.appendChild(chessPieceDiv);
+		chessPieceDiv.setAttribute('class', 'chessPieceDiv');
+		chessPieceDiv.innerHTML  = `<?xml version="1.0" standalone="no"?>
+		<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
+		<svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="1280.000000pt" height="972.000000pt" viewBox="0 0 1280.000000 972.000000"
+		 preserveAspectRatio="xMidYMid meet"> 
+		<g transform="translate(0.000000,972) scale(0.100000,-0.100000)" fill="#f44336" stroke="none"> 
+		${chessPiece.outerHTML}	</g></svg>`;
+	}
     
     initializeLoader() {
 		this.toggleVisibility(this.element, 'visible');
@@ -86,24 +103,34 @@ class Loader extends TheDOM {
 		this.height = this.element.getBoundingClientRect().height;
 		// console.log('LOADER WIDTH: ', this.width);
 		// console.log('LOADER HEIGHT: ', this.height);
-
-		this.element.innerHTML = loaderPicHTML;
-		// console.log(this.element.innerHTML);
 		
-		// this.element.style.backgroundPosition = `${this.width} center`;
-		// this.element.style.backgroundImage = `url('${loaderPic}')`;
-		// this.element.style.backgroundRepeat = "no-repeat";
-		// this.element.style.backgroundSize = this.width + 'px';		
+		console.log('IMPORTED ITEMS: ', this.allImportedItems.length);
 		// // this.consoleInitialInfo();
 		this.resizeGameWindow();
 		window.addEventListener('resize', this.resizeGameWindow);
-		for (let i = 0; i < this.importedPicReferences.length; i++) {
-			this.loadImage(this.importedPicReferences[i]);
-			//loadImage returns an image, use it if needed
-		}
+		this.loadAllItems();
+		this.insertPiece("king1", this.element);
+		this.insertPiece("queen1", this.element);
+		this.insertPiece("bishop1", this.element);		
+		this.insertPiece("knight1", this.element);
+		this.insertPiece("rook1", this.element);
+		this.insertPiece("pawn1", this.element);
+		this.insertPiece("pawn2", this.element);		
+		this.insertPiece("rook2", this.element);		
+		this.insertPiece("knight2", this.element);		
+		this.insertPiece("bishop2", this.element);		
+		this.insertPiece("queen2", this.element);		
+		this.insertPiece("king2", this.element);	
     }
 }
 
 export const loader = new Loader();
 
 export const ITEMSLOADED_EVENT_NAME = 'itemsLoaded';
+
+
+//REMOVE THIS:
+// this.element.style.backgroundPosition = `${this.width} center`;
+// this.element.style.backgroundImage = `url('${loaderPic}')`;
+// this.element.style.backgroundRepeat = "no-repeat";
+// this.element.style.backgroundSize = this.width + 'px';	
